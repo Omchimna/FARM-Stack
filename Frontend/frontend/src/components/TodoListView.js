c628fb7c25f7affb5cf4842799d9451f06c28b07
Om Chimna <110689335+Omchimna@users.noreply.github.com>
2025-01-31 23:33:21 +0530

    Update TodoListView.js 6

--- Frontend/frontend/src/components/TodoListView.js
+++ Frontend/frontend/src/components/TodoListView.js
@@ -2,20 +2,20 @@ import React from 'react';
 import TodoItem from './Todo';
 
 function TodoListView(props) {
-  const { todolist, loading, onDelete } = props; 
+  const { todolist, loading, onDelete } = props;
 
-  if (loading) { 
+  if (loading) {
     return <p>Loading tasks...</p>;
   }
 
-  if (todolist && todolist.length === 0) { 
+  if (todolist.length === 0) { 
     return <p>No tasks yet!</p>;
   }
 
   return (
     <div>
       <ul>
-        {todolist.map((todo, index) => ( 
+        {todolist.map((todo, index) => (
           <TodoItem key={index} todo={todo} onDelete={onDelete} />
         ))}
       </ul>
